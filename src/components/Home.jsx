d49a4509986cad213ce55334de28472ada058d75
Andrew Towner <keytonic@gmail.com>
2025-02-20 13:34:38 -0500

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ export default function Home()
 {
     return (
         <>
-            {getCookie("email") == null ? <LoginRegister /> : <><Navbar /><Body /><Footer /></>}
+            {getCookie("email") == null ? <LoginRegister action="login"/> : <><Navbar /><Body /><Footer /></>}
         </>
     );
 }

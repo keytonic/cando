f33179d6ac5b36dc4fef341383eeaec2714d48bf
Andrew Towner <keytonic@gmail.com>
2025-02-23 10:48:36 -0500

    commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -30,7 +30,56 @@ export default function Body()
     return (
         <>
             <div className="body-wrapper">
-                <span>Welcome home!</span><br /><br /><br /><br />
+                <div id="add-todo-wrapper">
+                    <div id="add-icon-cell">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 16 16">
+                            <path d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                        </svg>
+                    </div>
+                    <div id="add-text-cell">
+                        <input id="add-text-input" type="text" placeholder='Add a to-do...' />
+                    </div>
+                </div>
+                
+                <div id="todo-list-wrapper">
+                    <div className="list-item-card">
+                        <div className="check-cell">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="grey" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                            </svg>
+                        </div>
+                        <div className="text-cell"><span className="task-text">A long sentance to test out blah this shit ass fuck dick tits ass. A long sentance to test out blah this shit ass fuck dick tits ass. A long sentance to test out blah this shit ass fuck dick tits ass. A long sentance to test out blah this shit ass fuck dick tits ass. </span></div>
+                    </div>
+
+                    <div className="list-item-card">
+                        <div className="check-cell">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="grey" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            </svg>
+                        </div>
+                        <div className="text-cell"><span className="task-text">A long sentance to test out blah this shit ass fuck dick tits ass. A long sentance to test out blah this shit ass fuck dick tits ass. A long sentance to test out blah this shit ass fuck dick tits ass. A long sentance to test out blah this shit ass fuck dick tits ass. </span></div>
+                    </div>
+
+                    <div className="list-item-card">
+                        <div className="check-cell">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="grey" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            </svg>
+                        </div>
+                        <div className="text-cell"><span className="task-text">Milk </span></div>
+                    </div>
+
+
+
+                </div>
+                
+                
+                
+                
+                
+                
+                <br /><br /><br /><br />
                 <button type='submit' onClick={handleForm}> Log out </button>
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import CandoText from '../assets/candotext.png'
 import StarCheck from '../assets/star-check.svg'
 
 
-export function setCookie(name, value, days) {
+function setCookie(name, value, days) {
     const date = new Date();
     date.setTime(date.getTime() + (days*24*60*60*1000));
     const expires = "expires=" + date.toUTCString();
@@ -17,7 +17,7 @@ export function setCookie(name, value, days) {
     console.log(`cookie set, name: ${name}, value: ${value}, days: ${days}`);
 }
 
-export function getCookie(name) {
+function getCookie(name) {
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       let cookie = cookies[i].trim();
@@ -87,7 +87,7 @@ export default function Navbar()
                         <path d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5zM2 7h1v1H2zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm1 .5H2v1h1z"/>
                     </svg>
                 </div>
-                <div className="nav-cell">Title</div>
+                <div className="nav-cell title-cell"><span id="span-title-id">Current List Title</span></div>
                 <div className="nav-cell">
                     <svg className="nav-icon" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="white"  viewBox="0 0 16 16">
                         <path d="M10.5 1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4H1.5a.5.5 0 0 1 0-1H10V1.5a.5.5 0 0 1 .5-.5M12 3.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m-6.5 2A.5.5 0 0 1 6 6v1.5h8.5a.5.5 0 0 1 0 1H6V10a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5M1 8a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 1 8m9.5 2a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V13H1.5a.5.5 0 0 1 0-1H10v-1.5a.5.5 0 0 1 .5-.5m1.5 2.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
